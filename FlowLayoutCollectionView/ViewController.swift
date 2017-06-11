befcc86684d5b54c45afa6163b7ded19e576afc7
akatsuki174 <dev2015js@gmail.com>
2017-06-12 00:35:50 +0900

    UICollectionVIewControllerではなくUIViewControllerに変更

--- FlowLayoutCollectionView/ViewController.swift
+++ FlowLayoutCollectionView/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,12 +22,12 @@ class ViewController: UICollectionViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 50
     }
     
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         cell.backgroundColor = UIColor.brown
         
